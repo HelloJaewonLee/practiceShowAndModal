d21e01dd8cb3d3df766d0cf55ae82d92574f94eb
jaewon <wodnjszz1@naver.com>
2022-03-28 14:20:46 +0900

    Initial Commit

---
+++ practiceShowAndModal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceShowAndModal
+//
+//  Created by HENRY on 2022/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
